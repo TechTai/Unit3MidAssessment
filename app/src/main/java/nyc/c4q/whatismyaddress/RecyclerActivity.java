b496f314ded66db1eebadc33cf886b1f2f7e0a41
T.Nehisi <tariquanehisi@ac.c4q.nyc>
2017-11-21 20:43:02 -0500

    part01 commit

---
+++ app/src/main/java/nyc/c4q/whatismyaddress/RecyclerActivity.java
@@ -0,0 +1,15 @@
+package nyc.c4q.whatismyaddress;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class RecyclerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+    }
+
+}
